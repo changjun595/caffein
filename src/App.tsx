daaf3f7a0207c697f612fc530c5a4cab60f6e302
changjun Yun <33312259+changjun595@users.noreply.github.com>
2022-05-29 22:50:15 +0900

    [feat]App.tsx button 누르면 hello 나오게 하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const handleClick = () => {
+    const divContent = document.getElementById("helloDiv");
+
+    divContent!.innerHTML = "Hello"; 
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button id="helloButton" onClick={handleClick}>Click Me</button>
+      <div id="helloDiv"></div>
     </div>
   );
 }
