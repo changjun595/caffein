31acab0389a5c59054f4af0975ecfcd5bd4e8088
changjun Yun <33312259+changjun595@users.noreply.github.com>
2022-06-10 22:05:47 +0900

    [feat] HomBanner UI 업데이트

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -1,22 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
 const HomeBanner = () =>{
     return (
-        <div style={{
-            background : "url(https://raw.githubusercontent.com/congchu/caffein/week3/src/assets/images/home_header_bg.jpg)",
-            position : "absolute",
-            width : "960px",
-            height : "100px",
-            left : "240px",
-       }}>
-        <h2 style={{
-            marginTop : "33px",
-            color : "white",
-            textAlign : "center"
-
-        }}>
-            현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝
-        </h2>
-       </div>
+        <Container>
+            <div>
+                <h2>현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝</h2>
+            </div>
+       </Container>
     );
 };
 
 export default HomeBanner;
+
+const Container = styled.div`
+    position: relative;
+    width: 1020px;
+    height: 100px;
+    display: flex;
+    background: url("https://raw.githubusercontent.com/congchu/caffein/week3/src/assets/images/home_header_bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: white;
+    margin-left: auto;
+    margin-right : auto;
+
+    h2{
+        position: absolute;
+        padding-left: 42px;
+        top: 50%;
+        margin: 0;
+        transform: translateY(-50%);
+    }
+
+`
