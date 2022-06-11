28f8f9ff89307ee3b7dc93b3e1b998d0769825c2
changjun Yun <33312259+changjun595@users.noreply.github.com>
2022-06-11 23:13:42 +0900

    [feat]HomeBanner 반응형 UI 업데이트

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { maxWidth } from "../../../style/widthmax";
 
 const HomeBanner = () =>{
     return (
         <Container>
             <div>
-                <h2>현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝</h2>
+                <h2 className="homebanner_text">
+                    <div className="text_first">현직자와 소통하며 배우는</div>
+                    <div className="test_second">실무 스킬, 퍼스널 트레이닝</div>
+                </h2>
             </div>
        </Container>
     );
@@ -27,11 +31,16 @@ const Container = styled.div`
     margin-right : auto;
 
     h2{
+        display: flex;
         position: absolute;
         padding-left: 42px;
         top: 50%;
         margin: 0;
         transform: translateY(-50%);
     }
-
+    @media (max-width: 960px) {
+        .homebanner_text{
+            flex-direction: column;
+        }
+    }
 `
